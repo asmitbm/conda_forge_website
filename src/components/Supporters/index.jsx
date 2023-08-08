89adca78c79e8220dea8ce8199a5fa971fb54bd3
Asmit Malakannawar <asmitbm2952002@gmail.com>
2023-08-08 19:21:27 +0530

    add other supporters

--- src/components/Supporters/index.jsx
+++ src/components/Supporters/index.jsx
@@ -1,7 +1,96 @@
 import React from 'react';
 import styles from './styles.module.css';
 import Link from '@docusaurus/Link';
+import ThemedImage from '@theme/ThemedImage';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 import NumFOCUS from '@site/static/img/supporters/numfocus.svg';
+
+/* data of supporters */
+const financial = [
+    {
+        name: 'Nvidia',
+        link: 'https://www.nvidia.com/',
+        light: 'img/supporters/nvidia_light.svg',
+        dark: 'img/supporters/nvidia_dark.svg',
+        width: 250,
+    },
+];
+
+const infrastructure = [
+    {
+        name: 'Anaconda',
+        link: 'https://www.anaconda.com/',
+        light: 'img/supporters/anaconda_light.svg',
+        dark: 'img/supporters/anaconda_dark.svg',
+        width: 250,
+    },
+    {
+        name: 'Open Source Lab',
+        link: 'https://osuosl.org/',
+        light: 'img/supporters/osl_light.svg',
+        dark: 'img/supporters/osl_dark.svg',
+        width: 150,
+    },
+    {
+        name: 'OVHcloud',
+        link: 'https://www.ovhcloud.com/',
+        light: 'img/supporters/ovh_light.svg',
+        dark: 'img/supporters/ovh_dark.svg',
+        width: 250,
+    },
+    {
+        name: 'Digital Ocean',
+        link: 'https://www.digitalocean.com/',
+        light: 'img/supporters/do_light.svg',
+        dark: 'img/supporters/do_dark.svg',
+        width: 250,
+    },
+    {
+        name: 'Oracle Cloud',
+        link: 'https://www.oracle.com/cloud/',
+        light: 'img/supporters/oracle_light.svg',
+        dark: 'img/supporters/oracle_dark.svg',
+        width: 200,
+    },
+];
+
+const developer = [
+    {
+        name: 'Anaconda',
+        link: 'https://www.anaconda.com/',
+        light: 'img/supporters/anaconda_light.svg',
+        dark: 'img/supporters/anaconda_dark.svg',
+        width: 250,
+    },
+    {
+        name: 'Nvidia',
+        link: 'https://www.nvidia.com/',
+        light: 'img/supporters/nvidia_light.svg',
+        dark: 'img/supporters/nvidia_dark.svg',
+        width: 250,
+    },
+    {
+        name: 'Voltron Data',
+        link: 'https://voltrondata.com/',
+        light: 'img/supporters/voltron_light.svg',
+        dark: 'img/supporters/voltron_dark.svg',
+        width: 250,
+    },
+    {
+        name: 'Quansight Labs',
+        link: 'https://labs.quansight.org/',
+        light: 'img/supporters/quansight_light.svg',
+        dark: 'img/supporters/quansight_dark.svg',
+        width: 250,
+    },
+    {
+        name: 'QuantStack',
+        link: 'https://quantstack.net/',
+        light: 'img/supporters/quantstack_light.svg',
+        dark: 'img/supporters/quantstack_dark.svg',
+        width: 250,
+    },
+];
 
 export default function Supporters() {
     return (
@@ -17,6 +106,74 @@ export default function Supporters() {
                 <div className={styles.about_numfocus}>
                     <h3>conda-forge is a <span className="gradient_text">fiscally sponsored</span> project of NumFOCUS.</h3>
                     <p>A nonprofit dedicated to supporting the open source scientific computing community.</p>
+                </div>
+            </div>
+            <div className={styles.other_supporters}>
+                <div className={styles.supporters_card}>
+                    <div className={styles.supporters_conda_forge}>
+                        <h2><span className="gradient_text">Financial</span> Support</h2>
+                    </div>
+                    <div className={styles.card}>
+                        {financial.map(({ name, link, light, dark, width }, index) => (
+                            <div className={styles.cardWrapper} key={index}>
+                                <Link to={link}>
+                                    <ThemedImage className={styles.image}
+                                        alt={name}
+                                        sources={{
+                                            light: useBaseUrl(`${light}`),
+                                            dark: useBaseUrl(`${dark}`),
+                                        }}
+                                        width={width}
+                                    />
+                                </Link>
+                            </div >
+                        ))
+                        }
+                    </div>
+                </div>
+                <div className={styles.supporters_card}>
+                    <div className={styles.supporters_conda_forge}>
+                        <h2><span className="gradient_text">Infrastructure</span> Support</h2>
+                    </div>
+                    <div className={styles.card}>
+                        {infrastructure.map(({ name, link, light, dark, width }, index) => (
+                            <div className={styles.cardWrapper} key={index}>
+                                <Link to={link}>
+                                    <ThemedImage className={styles.image}
+                                        alt={name}
+                                        sources={{
+                                            light: useBaseUrl(`${light}`),
+                                            dark: useBaseUrl(`${dark}`),
+                                        }}
+                                        width={width}
+                                    />
+                                </Link>
+                            </div >
+                        ))
+                        }
+                    </div>
+                </div>
+                <div className={styles.supporters_card}>
+                    <div className={styles.supporters_conda_forge}>
+                        <h2><span className="gradient_text">Developer</span> Support</h2>
+                    </div>
+                    <div className={styles.card}>
+                        {developer.map(({ name, link, light, dark, width }, index) => (
+                            <div className={styles.cardWrapper} key={index}>
+                                <Link to={link}>
+                                    <ThemedImage className={styles.image}
+                                        alt={name}
+                                        sources={{
+                                            light: useBaseUrl(`${light}`),
+                                            dark: useBaseUrl(`${dark}`),
+                                        }}
+                                        width={width}
+                                    />
+                                </Link>
+                            </div >
+                        ))
+                        }
+                    </div>
                 </div>
             </div>
         </div>
